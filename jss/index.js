0bf65087ea8dc8dab7dbadbd45e09c1c37984b73
Parker <96428875+nlparker25@users.noreply.github.com>
2022-01-03 23:32:36 -0500

    Weather app
    Correct Tuesday abbreviation

--- jss/index.js
+++ jss/index.js
@@ -70,7 +70,7 @@ function getForecast(coordinates) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
